a9788d4fa74cbdaafb6a7fa49397b2e5ce89136b
AngKokLeong <ang.kok.leong.work@gmail.com>
2023-12-08 08:45:05 +0000

    added Item class

---
+++ class/item.py
@@ -0,0 +1,13 @@
+class Item:
+
+    item_id: str
+    item_name: str
+    price: float
+    quantity: int
+
+    def __init__(self, item_id: str, item_name:str, price:float, quantity:int):
+        self.item_id = item_id
+        self.item_name = item_name
+        self.price = price
+        self.quantity = quantity
+            
