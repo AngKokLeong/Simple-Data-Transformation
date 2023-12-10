a5e426e55380cea3e4f58e5211cdbe18099cf17f
AngKokLeong <ang.kok.leong.work@gmail.com>
2023-12-10 09:22:57 +0000

    SDT-003 implemented the json output function in this project with the given Order data

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from interface.file_processor import File_Processor
-from files.csv_file_processor import CSV_File_Processor
-
+from files.json_file_processor import JSON_File_Processor
+from entity.order import Order
 
 def main():
     #Data_Transformation_Driver.execute()
 
-    file_processor: File_Processor = CSV_File_Processor()
-    file_processor.read_file()
+    order_data: Order = Order('456789', 'asdasd', '123456', [], 56, 3)
+
+
+    file_processor: File_Processor = JSON_File_Processor()
+    file_processor.generate_file(order_data)
 
 
 
