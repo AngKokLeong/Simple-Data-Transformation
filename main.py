35dbe2db044f5ac1a4c3a40dfeb7ae1311f354f8
AngKokLeong <ang.kok.leong.work@gmail.com>
2023-12-11 06:05:59 +0000

    SDT-006 implement Order Data Processor to process one Order object at a time

--- main.py
+++ main.py
@@ -1,29 +1,20 @@
 from interface.data_processor import Data_Processor
-from data_processor.item_data_processor import Item_Data_Processor
+from data_processor.order_data_processor import Order_Data_Processor
 from entity.item import Item
+from entity.order import Order
 
-from interface.endpoint_data_retrieval import Endpoint_Data_Retrieval
-from data.item_data_retrieval import Item_Data_Retrieval
 
 def main():
     #Data_Transformation_Driver.execute()
 
-    endpoint_data_retrieval: Endpoint_Data_Retrieval = Item_Data_Retrieval()
+    order_one: Order = Order("4269010", "Daisy Duck", "156389", [Item("7", "White Gold Plated Princess", 9.99, 1), Item("17", "Rain Jacket Women Windbreaker Striped Climbing", 39.99, 3)], 0.0, 0)
+    
 
+    data_processor: Data_Processor = Order_Data_Processor()
+    result_order_object: Order = data_processor.process_data(order_one)
 
-    item_object_from_csv: Item = Item("17", "", "", 3)
-    item_object_from_fakestoreapi: Item = endpoint_data_retrieval.retrieve_data(item_object_from_csv.item_id)
-
-
-    data_processor: Data_Processor = Item_Data_Processor()
-
-    result:Item = data_processor.merge_data(item_object_from_csv, item_object_from_fakestoreapi)
-
-    print(result.item_id)
-    print(result.item_name)
-    print(result.price)
-    print(result.quantity)
-
+    print(result_order_object.total_price)
+    print(result_order_object.unique_items)
 
 if __name__ == "__main__":
     main()
