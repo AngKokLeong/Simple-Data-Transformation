50c2aa74044303b0c5b7e4d38f6ca9f59a2f6e35
AngKokLeong <ang.kok.leong.work@gmail.com>
2023-12-11 12:38:14 +0000

    SDT-007 enhanced item_data_processor class to retrieve item data from fakestoreapi and merge both item data object

--- data_processor/item_data_processor.py
+++ data_processor/item_data_processor.py
@@ -1,24 +1,38 @@
 from interface.data_processor import Data_Processor
 from entity.item import Item
-from collections import ChainMap
+from entity.order import Order
+from interface.endpoint_data_retrieval import Endpoint_Data_Retrieval
+from data.item_data_retrieval import Item_Data_Retrieval
 
 class Item_Data_Processor(Data_Processor):
 
-    def merge_data(self, item_object_one: Item, item_object_two: Item) -> Item:
+    item_data_retrieval: Endpoint_Data_Retrieval = Item_Data_Retrieval()
+
+    def process_data(self, item_id: str, item_object: Item) -> Item:
+        # process item data list based on one order number in the unique order number list
+        if item_object != None:
+            item_data_object_from_fake_store_api: Item = self.__create_item_from_fake_store_api(item_id)
+            return self.__merge_item_data_object(item_object, item_data_object_from_fake_store_api)
         
-            if item_object_one.item_name == '' and item_object_one.price == '':
-                item_object_one.item_name = item_object_two.item_name
-                item_object_one.price = item_object_two.price
+        return None
+
+    def __merge_item_data_object(self, item_object_one: Item, item_object_two: Item) -> Item:
+        
+        if item_object_one.item_name == '' and item_object_one.price == '':
+            item_object_one.item_name = item_object_two.item_name
+            item_object_one.price = item_object_two.price
     
-                return item_object_one
+            return item_object_one
             
-            elif item_object_two.item_name == '' and item_object_two.price == '':
-                item_object_two.item_name = item_object_one.item_name
-                item_object_two.price = item_object_one.price
+        elif item_object_two.item_name == '' and item_object_two.price == '':
+            item_object_two.item_name = item_object_one.item_name
+            item_object_two.price = item_object_one.price
     
-                return item_object_two
+            return item_object_two
 
-
+    def __create_item_from_fake_store_api(self, id: str) -> Item:
+        new_item_object: Item = self.item_data_retrieval.retrieve_data(id)
+        
+        return new_item_object
 
         
-
--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from interface.data_processor import Data_Processor
-from data_processor.order_data_processor import Order_Data_Processor
+from data_processor.item_data_processor import Item_Data_Processor
 from entity.item import Item
-from entity.order import Order
+
 
 
 def main():
     #Data_Transformation_Driver.execute()
 
+    item_data_processor: Data_Processor = Item_Data_Processor()
+    
+    result_item_object: Item = item_data_processor.process_data("1", Item(17, "", "", 3))
 
-    file_processor: File_Processor = CSV_File_Processor()
-    data = file_processor.read_file()
-    print(data)
-
-    #data_processor: Data_Processor = Order_Data_Processor()
-    #result_order_object: Order = data_processor.process_data(order_one)
-
-    #print(result_order_object.total_price)
-    #print(result_order_object.unique_items)
+    print(result_item_object.item_id)
+    print(result_item_object.item_name)
+    print(result_item_object.price)
+    print(result_item_object.quantity)
 
 if __name__ == "__main__":
     main()
