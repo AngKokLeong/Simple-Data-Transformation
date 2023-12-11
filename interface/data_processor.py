ea4029efc8606160c02bbea5ea7b7a95d021c5e9
AngKokLeong <ang.kok.leong.work@gmail.com>
2023-12-11 14:28:59 +0000

    SDT-008 implement Item List Data Processor class to generate item data object list for one order

--- interface/data_processor.py
+++ interface/data_processor.py
@@ -6,6 +6,9 @@ class Data_Processor:
 
     def process_data(self, data: object) -> object:
         pass
+
+    def process_data(self, data_object_one: object, data_object_two: object) -> object:
+        pass
     
 
     
